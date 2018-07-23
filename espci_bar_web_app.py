eff5a533e47292a155a68a5ec5928c3de2be289d
Samuel Diebolt <stryars@gmail.com>
2018-07-24 01:35:23 +0200

    Ignore logs.

--- espci_bar_web_app.py
+++ espci_bar_web_app.py
@@ -3,4 +3,6 @@ from app.models import User
 
 @app.shell_context_processor
 def make_shell_context():
+    """ Used to create the context when calling
+        flask shell. """
     return {'db': db, 'User': User}
